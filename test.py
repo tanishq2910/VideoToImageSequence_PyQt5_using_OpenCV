53b7d5c5ffe1e1432f02efd90156efd92a5474a5
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2023-05-01 15:35:48 +0530

    dependencies and models added

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+import time
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+
